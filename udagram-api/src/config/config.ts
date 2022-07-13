76afb666711fca69728f92e28458933d538687ec
DuyLT <luutheduy2@gmail.com>
2022-07-14 00:53:34 +0700

    update code to git

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'mypassword',
+  'database': 'postgres',
+  'host': 'mydbpro3.c1ql7fgcgter.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'project3-196005-dev',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
